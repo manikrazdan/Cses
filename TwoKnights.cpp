4de8a586747c2e4685e2c3b9d92cfa04b57a7df6
manikrazdan <116128219+manikrazdan@users.noreply.github.com>
2022-11-01 19:29:38 +0530

    Create TwoKnights.cpp

---
+++ TwoKnights.cpp
@@ -0,0 +1,12 @@
+ll f(ll n) {
+	ll ways=((n*n)*(n*n-1))/2; // no. of ways to place 2 knights on n*n chessboard
+  ll box=2*(n-1)*(n-2); // no. of 2*3 and 3*2 boxes in n*n chessboard
+  return ways-2*box;
+}
+void solve() {
+    ll n;
+    cin>>n;
+    for (ll i=1;i<=n;i++) {
+    	cout<<f(i)<<endl;
+    }
+}
